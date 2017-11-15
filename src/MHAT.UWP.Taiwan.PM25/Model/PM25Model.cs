02d1dc30aceb70e7821c7c4a7acffe784f456344
Alan Tsai <alan@alantsai.net>
2017-11-16 00:16:55 +0800

    加入測試binding

---
+++ src/MHAT.UWP.Taiwan.PM25/Model/PM25Model.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHAT.UWP.Taiwan.PM25.Model
+{
+    public class PM25Model
+    {
+        public string Site { get; set; }
+        public string county { get; set; }
+        public string PM25 { get; set; }
+        public string DataCreationDate { get; set; }
+        public string ItemUnit { get; set; }
+    }
+}
---
+++ src/MHAT.UWP.Taiwan.PM25/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MHAT.UWP.Taiwan.PM25.Model;
+
+namespace MHAT.UWP.Taiwan.PM25.ViewModel
+{
+    public class MainPageViewModel
+    {
+        public List<PM25Model> PM25Result { get; set; }
+
+        public MainPageViewModel()
+        {
+            PM25Result = new List<PM25Model>()
+            {
+                new PM25Model()
+                {
+                     PM25 = "10",
+                     county = "臺中市"
+                },
+                new PM25Model()
+                {
+                    PM25 = "20",
+                    county = "台北市"
+                }
+            };
+        }
+    }
+}
