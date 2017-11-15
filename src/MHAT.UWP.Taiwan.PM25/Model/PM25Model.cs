b84dabfa1971e4c59b4dfb5b87ffbcb805715575
Alan Tsai <alan@alantsai.net>
2017-11-16 00:33:20 +0800

    調整部分呈現

--- src/MHAT.UWP.Taiwan.PM25/Model/PM25Model.cs
+++ src/MHAT.UWP.Taiwan.PM25/Model/PM25Model.cs
@@ -13,5 +13,7 @@ namespace MHAT.UWP.Taiwan.PM25.Model
         public string PM25 { get; set; }
         public string DataCreationDate { get; set; }
         public string ItemUnit { get; set; }
+
+        public string PM25WithUnit { get { return $"{PM25} {ItemUnit}"; } }
     }
 }
